684938393cac52d26ae9664cc391e5f016705d39
WojWei2137 <jan.weiss2137@gmail.com>
2023-03-02 08:31:28 +0100

    dodanie pliku php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div>
+      egzamin
+    </div>
+  </body>
+</html>
